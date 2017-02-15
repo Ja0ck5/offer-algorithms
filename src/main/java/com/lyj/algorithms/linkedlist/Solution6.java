8fc6b98ccb685af1885801eda4f5e4bfde096aba
Ja0ck5 <975117619@qq.com>
2017-02-15 15:48:50 +0800

    complete 合并两个递增排序的链表

--- src/main/java/com/lyj/algorithms/linkedlist/Solution6.java
+++ src/main/java/com/lyj/algorithms/linkedlist/Solution6.java
@@ -1,5 +1,7 @@
 package com.lyj.algorithms.linkedlist;
 
+import java.awt.List;
+
 /**
  * 合并两个排序的链表
  * 输入两个递增排序的链表
@@ -8,5 +10,51 @@ package com.lyj.algorithms.linkedlist;
  *
  */
 public class Solution6 {
+	
+	public static class ListNode{
+		int val;
+		ListNode nxt;
+		public ListNode() {
+		}
+		public ListNode(int val) {
+			this.val = val;
+		}
+		@Override
+		public String toString() {
+			return "ListNode [val=" + val + ", nxt=" + nxt + "]";
+		}
+	}
+	
+	public static ListNode mergeListNode(ListNode ln1,ListNode ln2){
+		if(null == ln1)
+			return ln2;
+		if(null == ln2)
+			return ln1;
+		
+		ListNode mergement = null;
+		
+		if(ln1.val <ln2.val){
+			// 将链表 ln1 的一个元素赋值给 合并链表
+			mergement = ln1;
+			// 将下次比较后的链表的一个元素赋值给 合并链表“指针域”,即下一个
+			mergement.nxt = mergeListNode(ln1.nxt, ln2);
+		}else{
+			mergement = ln2;
+			mergement.nxt = mergeListNode(ln1, ln2.nxt);
+		}
+		return mergement;
+	}
+	
+	public static void main(String[] args) {
+		ListNode ln1 = new ListNode(1);
+		ln1.nxt = new ListNode(3);
+		ln1.nxt.nxt = new ListNode(5);
+		ListNode ln2 = new ListNode(2);
+		ln2.nxt = new ListNode(4);
+		ln2.nxt.nxt = new ListNode(6);
+		
+		System.out.println(mergeListNode(ln1, ln2));
+		
+	}
 
 }
