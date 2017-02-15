fd6d698aa4b8eefc4a7f6a62876e156098e06cca
Ja0ck5 <975117619@qq.com>
2017-02-15 17:38:22 +0800

    recursive mirrorTree

--- src/main/java/com/lyj/algorithms/tree/Solution2.java
+++ src/main/java/com/lyj/algorithms/tree/Solution2.java
@@ -7,5 +7,28 @@ package com.lyj.algorithms.tree;
  *
  */
 public class Solution2 {
+	
+	//¹¹½¨Ê÷
+	public static class BinaryTreeNode{
+		int val;
+		BinaryTreeNode left;
+		BinaryTreeNode right;
+		public BinaryTreeNode() {}
+		public BinaryTreeNode(int val, BinaryTreeNode left, BinaryTreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+		public BinaryTreeNode(int val) {
+			this.val = val;
+		}
+	}
+	
+	//recursiveMirrorTree
+	public static BinaryTreeNode recursiveMirrorTree(BinaryTreeNode btn){
+		
+		
+		return null;
+	}
 
 }
