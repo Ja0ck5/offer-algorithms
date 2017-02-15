35c0bbb023bfef0d199d14cedd613c8d7002c30b
Ja0ck5 <975117619@qq.com>
2017-02-15 18:53:52 +0800

    包含 min 函数的栈

---
+++ src/main/java/com/lyj/algorithms/number/Solution3.java
@@ -0,0 +1,12 @@
+package com.lyj.algorithms.number;
+
+/**
+ * 包含 min 函数的栈
+ * 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的min函数。
+ * 在该栈中，调用min、 push及pop德尔时间复杂度都是O(1)
+ * @author Ja0ck5
+ *
+ */
+public class Solution3 {
+
+}
