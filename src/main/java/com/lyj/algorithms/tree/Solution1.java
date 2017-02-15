a01c085c757ff9764a33af5b7676bf73d7b49025
Ja0ck5 <975117619@qq.com>
2017-02-15 15:58:15 +0800

    构建二叉树节点

--- src/main/java/com/lyj/algorithms/tree/Solution1.java
+++ src/main/java/com/lyj/algorithms/tree/Solution1.java
@@ -1,5 +1,6 @@
 package com.lyj.algorithms.tree;
 
+
 /**
  * 树的子结构
  * 输入两颗二叉树 A 和 B，判断 B 是不是 A 的子结构
@@ -7,5 +8,29 @@ package com.lyj.algorithms.tree;
  *
  */
 public class Solution1 {
+	public static class BinaryTreeNode {
+		// 节点值
+		 int val;
+		// 左节点
+		BinaryTreeNode left;
+		// 右节点
+		BinaryTreeNode right;
+
+		public BinaryTreeNode() {}
+
+		public BinaryTreeNode(int val) {
+			this.val = val;
+		}
+
+		@Override
+		public String toString() {
+			return "BinaryTreeNode [val=" + val + " left=" + left + ", right=" + right + "]";
+		}
+	    
+	}
+	public static boolean hasSubTree(BinaryTreeNode btn1,BinaryTreeNode btn2){
+		
+		return false;
+	}
 
 }
