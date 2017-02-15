5eef507db6b008b95c960d335f8c768203f1e4e9
Ja0ck5 <975117619@qq.com>
2017-02-15 16:48:49 +0800

    complete 二叉树的子结构

--- src/main/java/com/lyj/algorithms/tree/Solution1.java
+++ src/main/java/com/lyj/algorithms/tree/Solution1.java
@@ -26,11 +26,39 @@ public class Solution1 {
 		public String toString() {
 			return "BinaryTreeNode [val=" + val + " left=" + left + ", right=" + right + "]";
 		}
-	    
 	}
+	
 	public static boolean hasSubTree(BinaryTreeNode btn1,BinaryTreeNode btn2){
+		boolean flag = false;
+		if(null != btn1 && null != btn2){
+			// 根节点相同
+			if(btn1.val == btn2.val)
+				flag = doseTree1HaveTree2(btn1,btn2);
+			//根节点不同，则判断  左子节点
+			if(!flag)
+				flag = doseTree1HaveTree2(btn1.left, btn2);
+			if(!flag)
+				flag = doseTree1HaveTree2(btn1.right, btn2);
+		}
+		return flag;
+	}
+	
+	private static boolean doseTree1HaveTree2(BinaryTreeNode btn1, BinaryTreeNode btn2) {
+		//1 如果子树 btn2 为空，则 btn1 包含 btn2
+		if(null == btn2)
+			return true;
+		//2 如果树 1 为空则不包含树2
+		if( null == btn1)
+			return false;
+		//3 如果两节点值不相等，则不包含
+		if(btn1.val != btn2.val)
+			return false;
+		//4 判断对应的左右节点的值是否各自相等
+		return doseTree1HaveTree2(btn1.left, btn2.left) && doseTree1HaveTree2(btn1.right, btn2.right);
+	}
+	
+	public static void main(String[] args) {
 		
-		return false;
 	}
 
 }
