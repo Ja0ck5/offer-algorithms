8b0b8176cb55b46ce839d7777e8beeebed1b2246
Ja0ck5 <975117619@qq.com>
2017-02-15 17:47:04 +0800

    complete and test

--- src/main/java/com/lyj/algorithms/tree/Solution2.java
+++ src/main/java/com/lyj/algorithms/tree/Solution2.java
@@ -14,21 +14,43 @@ public class Solution2 {
 		BinaryTreeNode left;
 		BinaryTreeNode right;
 		public BinaryTreeNode() {}
-		public BinaryTreeNode(int val, BinaryTreeNode left, BinaryTreeNode right) {
-			this.val = val;
-			this.left = left;
-			this.right = right;
-		}
 		public BinaryTreeNode(int val) {
 			this.val = val;
 		}
+		@Override
+		public String toString() {
+			return "BinaryTreeNode [val=" + val + ", left=" + left + ", right=" + right + "]";
+		}
+		
 	}
 	
 	//recursiveMirrorTree
-	public static BinaryTreeNode recursiveMirrorTree(BinaryTreeNode btn){
+	public static BinaryTreeNode recursiveMirrorTree(BinaryTreeNode root){
+		if(null == root)
+			return null;
+		//交换节点的左右子节点
+		BinaryTreeNode tmpNode = root.left;
+		root.left = root.right;
+		root.right = tmpNode;
+		//递归。左右子节点
+		recursiveMirrorTree(root.left);
+		recursiveMirrorTree(root.right);
 		
+		return root;
+	}
+	
+	public static void main(String[] args) {
+		BinaryTreeNode root = new BinaryTreeNode(1);
+		root.left = new BinaryTreeNode(2);
+		root.right = new BinaryTreeNode(3);
 		
-		return null;
+		root.left.left = new BinaryTreeNode(4);
+		root.left.right = new BinaryTreeNode(5);
+		root.right.left = new BinaryTreeNode(6);
+		root.right.right = new BinaryTreeNode(7);
+		
+		System.out.println(root);
+		System.out.println(recursiveMirrorTree(root));
 	}
 
 }
