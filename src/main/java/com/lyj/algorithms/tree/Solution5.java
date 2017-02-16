411438be068fc1499bb9b8015c3751c35b9f36e5
Ja0ck5 <975117619@qq.com>
2017-02-16 14:59:22 +0800

    深度遍历

--- src/main/java/com/lyj/algorithms/tree/Solution5.java
+++ src/main/java/com/lyj/algorithms/tree/Solution5.java
@@ -3,6 +3,7 @@ package com.lyj.algorithms.tree;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
  * 二叉树中和为某一值的路径 输入一颗二叉树和一个整数，打印出二叉树中结点值的和为输入整数的所
  * 有路径。从树的根节点开始往下一直到叶结点所经过的结点形成一条路径。
@@ -12,7 +13,7 @@ import java.util.List;
  */
 public class Solution5 {
 	
-	class BinaryTreeNode {
+	public static class BinaryTreeNode {
 		int val;
 		BinaryTreeNode left;
 		BinaryTreeNode right;
@@ -33,18 +34,35 @@ public class Solution5 {
 	private List<ArrayList<Integer>> listAll = new ArrayList<ArrayList<Integer>>();
 	private List<Integer> list = new ArrayList<Integer>();
 
-	public List<ArrayList<Integer>> FindPath(BinaryTreeNode root, int target) {
-		
+	public  List<ArrayList<Integer>> findPath(BinaryTreeNode root, int target) {
 		if (root == null)
 			return listAll;
 		list.add(root.val);
+		//目标值减去经过的值
 		target -= root.val;
 		if (target == 0 && root.left == null && root.right == null)
 			listAll.add(new ArrayList<Integer>(list));
-		FindPath(root.left, target);
-		FindPath(root.right, target);
+		//查询左节点
+		findPath(root.left, target);
+		//查询右节点
+		findPath(root.right, target);
+		//移除最后一个元素,深度遍历完一条路径后要回退
 		list.remove(list.size() - 1);
 		return listAll;
+	}
+	
+	public static void main(String[] args) {
+		BinaryTreeNode root = new BinaryTreeNode(1);
+		root.left = new BinaryTreeNode(2);
+		root.right = new BinaryTreeNode(3);
+
+		root.left.left = new BinaryTreeNode(4);
+		root.left.right = new BinaryTreeNode(5);
+		root.right.left = new BinaryTreeNode(6);
+		root.right.right = new BinaryTreeNode(7);
+
+		Solution5 s5 = new Solution5();
+		System.out.println(s5.findPath(root,7));
 	}
 
 }
