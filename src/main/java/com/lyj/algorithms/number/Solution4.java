cce17c4985388609fe606ba435ecf1e01d5bffe4
Ja0ck5 <975117619@qq.com>
2017-02-17 15:35:20 +0800

    add english orignal comment

--- src/main/java/com/lyj/algorithms/number/Solution4.java
+++ src/main/java/com/lyj/algorithms/number/Solution4.java
@@ -16,6 +16,28 @@ public class Solution4 {
 	}
 
 	/** 
+	 *   Go through the digit positions by using position multiplier m with values 1, 10, 100, 1000, etc.
+	 *   
+	 *   For each position, split the decimal representation into two parts, 
+	 *   
+	 *   for example split n=3141592 into a=31415 and b=92 when we're at m=100 for analyzing the hundreds-digit. 
+	 *   
+	 *   And then we know that the hundreds-digit of n is 1 for prefixes "" to "3141", i.e., 3142 times. 
+	 *   
+	 *   Each of those times is a streak, though. 
+	 *   
+	 *   Because it's the hundreds-digit, each streak is 100 long. So (a / 10 + 1) * 100 times, the hundreds-digit is 1.
+	 *   
+	 *   Consider the thousands-digit, i.e., when m=1000. 
+	 *   
+	 *   Then a=3141 and b=592. The thousands-digit is 1 for prefixes "" to "314", so 315 times. 
+	 *   
+	 *   And each time is a streak of 1000 numbers. However, since the thousands-digit is a 1, the very last streak isn't 1000 numbers but only 593 numbers, for the suffixes "000" to "592". So (a / 10 * 1000) + (b + 1) times, the thousands-digit is 1.
+	 *   
+	 *   The case distincton between the current digit/position being 0, 1 and >=2 can easily be done in one expression. 
+	 *   
+	 *   With (a + 8) / 10 you get the number of full streaks, and a % 10 == 1 tells you whether to add a partial streak.
+	 *   
 	 *   主要思路：设定整数点（如1、10、100等等）作为位置点i（对应n的各位、十位、百位等等），分别对每个数位上有多少包含1的点进行分析
 	 * 
 	 *   根据设定的整数位置，
@@ -33,6 +55,8 @@ public class Solution4 {
 	 *   综合以上三种情况，当百位对应0或>=2时，有(a+8)/10次包含所有100个点，还有当百位为1(a%10==1)，需要增加局部点b+1
 	 *    
 	 *   之所以补8，是因为当百位为0，则a/10==(a+8)/10，(310+8)/10  (314+8)/10 效果等同于 以上的【1】【2】,当百位>=2，补8会产生进位位，效果等同于(a/10+1)
+	 *   
+	
 	 */
 	public int countDigitOne(int n) {
 		int ones = 0;
