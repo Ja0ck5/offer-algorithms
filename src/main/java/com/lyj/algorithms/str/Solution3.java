beb681ba7febc4939b0deb9af9decccdccbdcad6
Ja0ck5 <975117619@qq.com>
2017-02-18 12:53:24 +0800

    输入一个英文句子，翻转句子中单词的顺序，但单词内字啊的顺序不变。 为简单起见，标点符号和普通字母一样处理。

--- src/main/java/com/lyj/algorithms/str/Solution3.java
+++ src/main/java/com/lyj/algorithms/str/Solution3.java
@@ -27,4 +27,51 @@ public class Solution3 {
 		}
 		System.out.println(sb);
 	}
+
+	/**
+	 * 将data中start到end之间的数字反转
+	 *
+	 * @param data
+	 * @param start
+	 * @param end
+	 */
+	public static void reverse(char[] data, int start, int end) {
+		if (data == null || data.length < 1 || start < 0 || end > data.length || start > end)
+			return;
+		while (start < end) {
+			char tmp = data[start];
+			data[start] = data[end];
+			data[end] = tmp;
+			start++;
+			end--;
+		}
+	}
+
+	/**
+	 * 输入一个英文句子，翻转句子中单词的顺序，但单词内字啊的顺序不变。 为简单起见，标点符号和普通字母一样处理。
+	 *
+	 * @param data
+	 * @return
+	 */
+	public static char[] reverseSentence(char[] data) {
+		if (data == null || data.length < 1) {
+			return data;
+		}
+		reverse(data, 0, data.length - 1);
+		int start = 0;
+		int end = 0;
+		while (start < data.length) {
+			if (data[start] == ' ') {
+				start++;
+				end++;
+			} else if (end == data.length || data[end] == ' ') {
+				reverse(data, start, end - 1);
+				end++;
+				start = end;
+			} else {
+				end++;
+			}
+		}
+		return data;
+	}
 }
