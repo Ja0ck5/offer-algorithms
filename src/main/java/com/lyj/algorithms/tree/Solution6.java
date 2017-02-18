e43b064a20268b958c3bd1c177f142fcc444d433
Ja0ck5 <975117619@qq.com>
2017-02-18 10:20:54 +0800

    二叉树的深度

---
+++ src/main/java/com/lyj/algorithms/tree/Solution6.java
@@ -0,0 +1,15 @@
+package com.lyj.algorithms.tree;
+
+/**
+ * 二叉树的深度 
+ * 
+ * 输入一棵二叉树的根结点，求该树的深度。
+ * 
+ * 从根结点到叶结点依次经过 的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度
+ * 
+ * @author Ja0ck5
+ *
+ */
+public class Solution6 {
+
+}
