8e031af5db7a9b9929290ff6198feb6498a8a513
Ja0ck5 <975117619@qq.com>
2017-02-18 10:38:44 +0800

    输入一棵二叉树的根结点，判断该树是不是平衡二叉树

---
+++ src/main/java/com/lyj/algorithms/tree/Solution7.java
@@ -0,0 +1,13 @@
+package com.lyj.algorithms.tree;
+
+/**
+ * 输入一棵二叉树的根结点，判断该树是不是平衡二叉树。
+ * 
+ * 如果某二叉树 中任意结点的左右子树的深度相差不超过1，那么他就是一棵平衡二叉树。
+ * 
+ * @author Ja0ck5
+ *
+ */
+public class Solution7 {
+
+}
